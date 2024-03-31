2998384f827c8093a6adfd044592166cdaa50da1
horus018 <rubira820@gmail.com>
2024-03-31 20:45:17 -0300

    Setup project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.contaspagarlucasrubira"
+    namespace = "com.example.contaspagarandroid"
     compileSdk = 34
 
     defaultConfig {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ContasPagarLucasRubira"
+rootProject.name = "ContasPagarAndroid"
 include(":app")
  
